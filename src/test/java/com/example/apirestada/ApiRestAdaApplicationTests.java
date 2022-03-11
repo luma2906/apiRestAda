0fcfa63132927dc2eda90ef351d6d913230db7ff
luma <luma2906@gmail.com>
2022-03-10 22:59:01 -0500

    first commit

---
+++ src/test/java/com/example/apirestada/ApiRestAdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestAdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
